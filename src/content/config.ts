7c9b667f05e217b2134acd1a639770b3abfe16d7
Youssefh71 <youssefhourri69@gmail.com>
2024-01-08 14:57:04 +0100

    suppresion descirption

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,6 @@ export const collections = {
 		type: 'content',
 		schema: z.object({
 			title: z.string(),
-			description: z.string(),
 			publishDate: z.coerce.date(),
 			tags: z.array(z.string()),
 			img: z.string(),
--- src/content/work/markdown-mystery-tour.md
+++ src/content/work/markdown-mystery-tour.md
@@ -3,8 +3,6 @@ title: Artisans
 publishDate: 2020-03-02 00:00:00
 img: /assets/stock-1.jpg
 img_alt: Iridescent ripples of a bright blue and pink liquid
-description: |
-  
 tags:
   - PHP/SYmfony
   - JavaScript
